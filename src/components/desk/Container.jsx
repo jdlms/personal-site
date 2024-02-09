93f5cbf47446325cfb69658500dabc7126ccd97a
Joshua LaMorey-Salzmann <lamorey.joshua@gmail.com>
2024-02-09 15:29:43 +0100

    styles + text

--- src/components/desk/Container.jsx
+++ src/components/desk/Container.jsx
@@ -9,7 +9,7 @@ export function Container() {
     <div className="container flex justify-center items-center h-44 md:h-88">
       <Canvas
         style={{ height: "100%", width: "100%" }}
-        camera={{ position: [0, 5, -10], fov: 70 }}
+        camera={{ position: [0, 7, -10], fov: 70 }}
       >
         <ambientLight intensity={2.4} />
         <pointLight position={[70, 50, 60]} />
@@ -21,7 +21,7 @@ export function Container() {
           enablePan={false}
           autoRotate={true}
           autoRotateSpeed={4.5}
-          target={[1.5, 2.9, 1]}
+          target={[2, 2.9, 1]}
         />
       </Canvas>
     </div>

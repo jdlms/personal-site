8d630d75fe2f481ff18b1297d2410853bb01572e
Joshua LaMorey-Salzmann <lamorey.joshua@gmail.com>
2024-02-11 21:14:14 +0100

    @astrojs/cloudflare installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: true,
-    }),
-    react(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: true
+  }), react()],
+  output: "server",
+  adapter: cloudflare()
 });

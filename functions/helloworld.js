24c57757921c4bf4f6e97d359e5d1aea7dfa0052
Joshua LaMorey-Salzmann <lamorey.joshua@gmail.com>
2024-03-08 11:20:24 +0100

    update

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -32,8 +32,6 @@
 // }
 
 export async function onRequest(context) {
-  const task = await context.env.jdlmsDotDev.get(
-    "totalWaves"
-  );
+  const task = await context.env.KV_STORE.get("totalWaves");
   return new Response(task);
 }

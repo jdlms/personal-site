e9960a4be94cf9b3ac716591d95bc521a1d80207
Joshua LaMorey-Salzmann <lamorey.joshua@gmail.com>
2024-03-25 20:52:29 +0100

    wave refactor

--- functions/wave.js
+++ functions/wave.js
@@ -1,30 +1,3 @@
-// export async function onRequest(context) {
-//   try {
-//     let totalWaves = await context.env.KV_STORE.get(
-//       "totalWaves"
-//     );
-
-//     totalWaves = totalWaves ? parseInt(totalWaves) : 0;
-
-//     totalWaves += 1;
-
-//     increment(context, totalWaves);
-
-//     return new Response(totalWaves.toString());
-//   } catch (error) {
-//     return new Response("Error processing waves request", {
-//       status: 500,
-//     });
-//   }
-// }
-
-// async function increment(context, waves) {
-//   await context.env.KV_STORE.put(
-//     "totalWaves",
-//     waves.toString()
-//   );
-// }
-
 export async function onRequest(context) {
   try {
     let totalWaves = await context.env.KV_STORE.get(

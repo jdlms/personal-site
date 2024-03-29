2edc7cada92a2dd96326cba147c0c70f862d89ba
Joshua LaMorey-Salzmann <lamorey.joshua@gmail.com>
2024-03-29 08:17:55 +0100

    starredposts fetch

--- functions/getBookmarks.js
+++ functions/getBookmarks.js
@@ -1,8 +1,20 @@
 export async function onRequest(context) {
   const ps = context.env.bookmarks.prepare(
-    "SELECT * from users"
+    "SELECT * from StarredPosts"
   );
   const data = await ps.all();
 
   return Response.json(data);
 }
+// export default {
+// 	async fetch(request, env) {
+// 		const { pathname } = new URL(request.url);
+
+// 		if (pathname === '/api/bookmarks') {
+// 			const { results } = await env.DB.prepare('SELECT * FROM StarredPosts').all();
+// 			return Response.json(results);
+// 		}
+
+// 		return new Response('Call /api/bookmarks to see get full bookmark list');
+// 	},
+// };

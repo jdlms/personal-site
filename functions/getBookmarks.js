8a419b7fb0ad33908945886b10f91ea5a3e08a77
Joshua LaMorey-Salzmann <lamorey.joshua@gmail.com>
2024-04-02 22:55:11 +0200

    .....

--- functions/getBookmarks.js
+++ functions/getBookmarks.js
@@ -4,8 +4,8 @@ export async function onRequestGet(context) {
 
   const response = await fetch(bookmarksUrl);
 
-  const data = await response.json(); // If the bookmarks Worker returns JSON
+  const data = await response.json();
   return new Response(JSON.stringify(data), {
-    headers: { "Content-Type": "application/json" }, // Ensure correct Content-Type
+    headers: { "Content-Type": "application/json" },
   });
 }

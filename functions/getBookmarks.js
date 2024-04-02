623921e23be2b93d1e4bd411d2b968db8b698a4e
Joshua LaMorey-Salzmann <lamorey.joshua@gmail.com>
2024-04-02 22:34:11 +0200

    getBookmarks refactor

--- functions/getBookmarks.js
+++ functions/getBookmarks.js
@@ -1,9 +1,3 @@
 export async function onRequestGet(context) {
-  const url = new URL(context.request.url);
-  url.pathname = "/api/bookmarks";
-
-  return context.env.getBookmarks.fetch(
-    url.toString(),
-    context.request
-  );
+  return context.env.getBookmarks.fetch(context.request);
 }

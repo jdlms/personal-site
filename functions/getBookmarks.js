10f91728a0df5c122a9cae3b11b14a636e860d9d
Joshua LaMorey-Salzmann <lamorey.joshua@gmail.com>
2024-04-02 22:51:19 +0200

    refactor

--- functions/getBookmarks.js
+++ functions/getBookmarks.js
@@ -1,3 +1,11 @@
 export async function onRequestGet(context) {
-  return context.env.getBookmarks.fetch(context.request);
+  const bookmarksUrl =
+    "https://bookmarks.lamorey-joshua.workers.dev/";
+
+  const response = await fetch(bookmarksUrl);
+
+  const data = await response.json(); // If the bookmarks Worker returns JSON
+  return new Response(JSON.stringify(data), {
+    headers: { "Content-Type": "application/json" }, // Ensure correct Content-Type
+  });
 }

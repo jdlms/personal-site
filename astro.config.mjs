06f224d70ae7bae0d47f8c7873c9eb47cc141c58
jdlms <lamorey.joshua@gmail.com>
2024-11-01 09:38:36 +0100

    fix: svg icons, astro-icon added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,14 @@ import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), react(), icon()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,

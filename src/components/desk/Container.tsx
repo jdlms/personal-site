a0da003baa49ccb493bfd3f32b1ec0dd4ee6f3f7
jdlms <lamorey.joshua@gmail.com>
2025-05-14 21:34:40 +0200

    spinner

--- src/components/desk/Container.tsx
+++ src/components/desk/Container.tsx
@@ -19,7 +19,7 @@ export function Container() {
           color="#fff"
           distance={0}
         />
-        <Suspense>
+        <Suspense fallback={<Spinner />}>
           <Loader />
         </Suspense>
         <OrbitControls
--- src/components/desk/Spinner.tsx
+++ src/components/desk/Spinner.tsx
@@ -4,22 +4,24 @@ import { Html } from "@react-three/drei";
 export function Spinner() {
   return (
     <Html prepend center>
-      <LineWave
-        visible={true}
-        height="100"
-        width="100"
-        firstLineColor="#ad7753"
-        middleLineColor="#ffd700"
-        lastLineColor="#ff6b6b"
-        ariaLabel="line-wave-loading"
-        wrapperStyle={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          marginBottom: "12rem",
-        }}
-        wrapperClass="spinner"
-      />
+      <div className="bg-neutral-800 w-44 h-44 md:h-64 md:w-64 cursor-pointer rounded-full  shadow-inner">
+        <LineWave
+          visible={true}
+          height="90"
+          width="90"
+          firstLineColor="#ad7753"
+          middleLineColor="#ffd700"
+          lastLineColor="#ff6b6b"
+          ariaLabel="line-wave-loading"
+          wrapperStyle={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginBottom: "12rem",
+          }}
+          wrapperClass="spinner"
+        />
+      </div>
     </Html>
   );
 }

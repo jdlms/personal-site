4ad287a9fa23f095740e4ad1efb4e0828ef42b0f
jdlms <lamorey.joshua@gmail.com>
2025-07-24 21:02:52 +0200

    spinner fix

--- src/components/desk/Container.tsx
+++ src/components/desk/Container.tsx
@@ -2,7 +2,6 @@ import { Suspense, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { Loader } from "./Loader";
-import { Spinner } from "./Spinner";
 
 export function Container() {
   useEffect(() => {
@@ -16,14 +15,15 @@ export function Container() {
         camera={{ position: [0, 7, -10], fov: 75 }}
         className="w-44 h-44 md:h-64 md:w-64 cursor-pointer rounded-full shadow-inner"
       >
-         <color attach="background" args={["#27272a"]} />
-         <ambientLight intensity={2} />        <pointLight
+        <color attach="background" args={["#27272a"]} />
+        <ambientLight intensity={2} />{" "}
+        <pointLight
           position={[0, 18, -10]}
           intensity={500}
           color="#fff"
           distance={0}
         />
-        <Suspense fallback={<Spinner />}>
+        <Suspense>
           <Loader />
         </Suspense>
         <OrbitControls
